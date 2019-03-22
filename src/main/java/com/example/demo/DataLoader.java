75ec4264d127e7c697bcc28f37f2e9c93acd6fad
Dagmawi Fasil <dougmc134@yahoo.com>
2019-03-22 15:58:04 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/DataLoader.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataLoader implements CommandLineRunner {
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    RoleRepository roleRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public void run(String... strings) throws Exception{
+
+    }
+}
---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,80 @@
+package com.example.demo;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@Controller
+public class HomeController {
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    MessageRepository messageRepository;
+
+    @RequestMapping("/")
+    public String index(Model model){
+        model.addAttribute("messages", messageRepository.findAll());
+        return "list";
+    }
+
+    @RequestMapping("/login")
+    public String login(){
+        return "login";
+    }
+
+
+    @GetMapping("/add")
+    public String messageForm(Model model){
+        model.addAttribute("message", new Message());
+        return "msgform";
+    }
+
+    @PostMapping("/process")
+    public String processForm(@Valid Message message, BindingResult result){
+        if(result.hasErrors()){
+            return "msgform";
+        }
+
+        messageRepository.save(message);
+        return "redirect:/";
+    }
+
+    @RequestMapping("/detail/{id}")
+    public String showMsg(@PathVariable("id") long id, Model model) {
+        model.addAttribute("message", messageRepository.findById(id).get());
+        return "show";
+    }
+
+    @RequestMapping("/update/{id}")
+    public String updateMsg(@PathVariable("id") long id, Model model){
+        model.addAttribute("message", messageRepository.findById(id).get());
+        return "msgform";
+    }
+
+    @RequestMapping(value = "/register", method = RequestMethod.GET)
+    public String showRegistrationPage(Model model){
+        model.addAttribute("user", new User());
+        return "registration";
+    }
+
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    public String processRegistrationPage(
+            @Valid @ModelAttribute("user") User user,
+            BindingResult result,
+            Model model){
+        model.addAttribute("user", user);
+        if(result.hasErrors()){
+            return "registration";
+        }else{
+            userService.saveUser(user);
+            model.addAttribute("message", "User Account Successfully Created");
+        }
+        return "redirect:/";
+    }
+}
